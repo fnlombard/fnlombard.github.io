bc92f95987e5a435764954c769e783d1b67b173d
francois <fnlombard@gmail.com>
2025-06-10 19:28:46 +0200

    Add first personal entry

--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -1,1 +1,13 @@
-<div>Lol... yes. this is personal.</div>
+<script lang="ts">
+    import Timeline from "$lib/components/timeline/Timeline.svelte";
+    import data from "$lib/data/personal.json";
+
+    const items: TimelineItem[] = data.map((item, index) => ({
+        id: index,
+        ...item
+    }));
+</script>
+
+<div class="px-8">
+    <Timeline {items} />
+</div>
