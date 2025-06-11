13eff1298c008fceb6714db51e19c1ddb9db3602
francois <fnlombard@gmail.com>
2025-06-11 19:49:22 +0200

    Sort items descending date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
     import Timeline from "$lib/components/timeline/Timeline.svelte";
     import data from "$lib/data/cv.json";
 
-    const items: TimelineItem[] = data.map((item, index) => ({
-        id: index,
-        ...item
-    }));
+    const items: TimelineItem[] = data
+        .map((item, index) => ({
+            id: index,
+            ...item
+        }))
+        .sort((a, b) => {
+            return new Date(b.date_start).getTime() - new Date(a.date_start).getTime();
+        });
 </script>
 
 <Timeline {items} />
