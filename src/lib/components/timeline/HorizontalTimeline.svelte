cae3bc8cf17bb099d6ca5fd3423923f8cbf31516
francois <fnlombard@gmail.com>
2025-06-13 23:36:50 +0200

    Rework and break everything. Nice.

--- src/lib/components/timeline/HorizontalTimeline.svelte
+++ src/lib/components/timeline/HorizontalTimeline.svelte
@@ -9,27 +9,93 @@
     }
 
     const props: timelineProps = $props();
-    let hoveredIndex: number | null = $state(null);
-
-    $effect(() => {
-        props.update_selected(hoveredIndex);
-    });
 
     const todayStr = new Date().toISOString().split("T")[0];
-
-    const normalizeDate = (dateStr: string) =>
-        dateStr.toLowerCase() === "current" ? todayStr : dateStr;
-
-    const toDate = (dateStr: string) => new Date(normalizeDate(dateStr)).getTime();
-
     const startTimes = props.items.map((item) => toDate(item.date_start));
 
     const minTime = Math.min(...startTimes);
     const maxTime = Date.now();
     const totalTime = maxTime - minTime;
 
+    interface TimelineItemVM extends TimelineItem {
+        left: number;
+        scale: number;
+        isHighlighted: boolean;
+    }
+
+    function normalizeDate(dateString: string): string {
+        return dateString.toLowerCase() === "current" ? todayStr : dateString;
+    }
+
+    function toDate(dateString: string): number {
+        return new Date(normalizeDate(dateString)).getTime();
+    }
+
+    function getItemLeft(item: TimelineItem): number {
+        const start = toDate(item.date_start);
+        return ((start - minTime) / totalTime) * 100;
+    }
+
+    const items: TimelineItemVM[] = $state(
+        props.items.map((item) => ({
+            ...item,
+            left: getItemLeft(item),
+            scale: 50,
+            isHighlighted: false
+        }))
+    );
+
+    /** Mouse Hover Logic */
+    let positionPercentage: number = $state(0);
+    let containerRef: HTMLDivElement | undefined = undefined;
+
+    let hoveredIndex: number | null = $state(null);
+    function handleMouseMove(event: MouseEvent): void {
+        if (containerRef === undefined) return;
+
+        const rect = containerRef.getBoundingClientRect();
+        positionPercentage = ((event.clientX - rect.left) / rect.width) * 100;
+
+        let selectedItem = items.at(0);
+
+        if (selectedItem === undefined) return;
+
+        let maxScale = -Infinity;
+
+        items.forEach((item) => {
+            const distance = Math.abs(item.left - positionPercentage);
+            const scale = Math.round(Math.max(100 - distance, 10));
+            item.scale = scale;
+
+            if (scale > maxScale) {
+                maxScale = scale;
+                selectedItem = item;
+            }
+        });
+
+        items.forEach((item) => {
+            if (item === selectedItem) item.isHighlighted = true;
+            else item.isHighlighted = false;
+        });
+
+        props.update_selected(items.indexOf(selectedItem));
+    }
+
+    function handleMouseLeave(event: MouseEvent): void {
+        items.forEach((item) => {
+            item.scale = 50;
+            item.isHighlighted = false;
+        });
+
+        props.update_selected(null);
+    }
+
+    $effect(() => {
+        props.update_selected(hoveredIndex);
+    });
+
     const getTickDates = (): { date: string; left: number }[] =>
-        props.items
+        items
             .map((item) => ({
                 date: item.date_start,
                 left: ((toDate(item.date_start) - minTime) / totalTime) * 100
@@ -43,12 +109,7 @@
         if (props.highlighted_id !== null) {
             return props.items.find((item) => item.id === props.highlighted_id) || null;
         }
-        return hoveredIndex !== null ? props.items[hoveredIndex] : null;
-    });
-
-    const selectedIndex = $derived.by(() => {
-        if (highlightedItem === null) return null;
-        return props.items.findIndex((item) => item.id === highlightedItem.id);
+        return items.find((item) => item.isHighlighted === true) || null;
     });
 
     const selectedItemWidth = $derived.by(() => {
@@ -67,7 +128,13 @@
     });
 </script>
 
-<div class="relative w-full px-4 pt-20 pb-4">
+<div
+    class="relative w-full px-4 pt-20 pb-4"
+    bind:this={containerRef}
+    onmousemove={handleMouseMove}
+    onmouseleave={handleMouseLeave}
+    role="presentation"
+>
     <div class="relative h-3 w-full">
         <!-- Base timeline -->
         <div class="absolute top-1/2 left-0 z-0 h-1 w-full -translate-y-1/2 bg-gray-900"></div>
@@ -85,16 +152,15 @@
 
         <!-- Entries -->
         <div class="absolute top-[calc(50%-40px)] left-0 w-full">
-            {#each props.items as item, i}
-                <HorizontalTimelineItem
-                    {item}
-                    index={i}
-                    nextStart={props.items[i + 1]?.date_start ?? null}
-                    {minTime}
-                    {maxTime}
-                    active={hoveredIndex === i || props.highlighted_id === item.id}
-                    onhover={(i) => (hoveredIndex = i)}
-                />
+            {#each items as item, i}
+                <div class="absolute" style="Left: {getItemLeft(item)}%">
+                    <HorizontalTimelineItem
+                        {item}
+                        index={i}
+                        active={item.isHighlighted || props.highlighted_id === item.id}
+                        scale={item.scale}
+                    />
+                </div>
             {/each}
         </div>
 
--- src/lib/components/timeline/HorizontalTimelineItem.svelte
+++ src/lib/components/timeline/HorizontalTimelineItem.svelte
@@ -1,46 +1,21 @@
 <script lang="ts">
     import Icon from "$lib/components/icon/Icon.svelte";
-    import { onMount } from "svelte";
 
     interface itemProps {
         item: TimelineItem;
-        nextStart: string | null;
         index: number;
-        minTime: number;
-        maxTime: number;
         active: boolean;
-        onhover: (t: number | null) => void;
+        scale: number;
     }
 
     const props: itemProps = $props();
-
-    const toDate = (d: string) => new Date(d).getTime();
-    const start = toDate(props.item.date_start);
-    const total = props.maxTime - props.minTime;
-    const left = ((start - props.minTime) / total) * 100;
-
-    let bubblePosition = $state("translate-x-1/9");
-    let flyX = $state("-200");
-    onMount(() => {
-        if (left > 70) {
-            bubblePosition = "-translate-x-68";
-            flyX = "200";
-        }
-    });
-
-    function mouseEnter(): void {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        props.onhover(props.index);
-    }
 </script>
 
 <button
     class="absolute cursor-pointer transition-all duration-300 {props.active
-        ? 'translate-y-1 scale-100'
-        : 'translate-y-4 scale-50'}"
-    style="left: {left}%;"
-    onmouseenter={() => mouseEnter()}
-    onmouseleave={() => props.onhover(null)}
+        ? 'translate-y-1'
+        : 'translate-y-3'}"
+    style="scale: {props.active ? '100' : props.scale}%;"
     onclick={() => {
         if (props.item.url) {
             window.open(props.item.url, "_blank");
